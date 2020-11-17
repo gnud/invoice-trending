7b4ba07b79697fde544d0f044e093e2b80b3d7ee
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-17 15:41:03 +0100

    Added test for API invoice list

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,5 +1,11 @@
-describe('Initial test', () => {
-    it('should test that true === true', () => {
-        expect(true).toBe(true)
+const request = require('supertest')
+const app = require('../app')
+
+describe('Invoice routes', () => {
+    it('should invoice list', async () => {
+        const res = await request(app)
+            .get('/invoice')
+            .send()
+        expect(res.statusCode).toEqual(200)
     })
 })
