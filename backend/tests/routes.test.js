3cff4c017d043c11cae6bcb30c6ac929ea61e208
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-17 19:30:17 +0100

    Added test for API invoice list json response

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const app = require('../app')
+const db = require('../models')
 
 describe('Invoice routes', () => {
     it('should invoice list', async () => {
@@ -7,5 +8,18 @@ describe('Invoice routes', () => {
             .get('/invoice')
             .send()
         expect(res.statusCode).toEqual(200)
+    })
+
+    it('should invoice list json response', async () => {
+        const res = await request(app)
+            .get('/invoice')
+            .send()
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.length).toBeGreaterThanOrEqual(1)
+    })
+
+    // After all tests have finished, close the DB connection
+    afterAll(async () => {
+        await db.sequelize.close()
     })
 })
