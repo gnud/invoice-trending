4395fa10dded33b40e3c2e17708560a30f3633e5
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-18 03:29:27 +0100

    Added test for API uploading CSV invoice

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const app = require('../app')
 const db = require('../models')
+const path = require('path')
 
 describe('Invoice routes', () => {
     it('should invoice list', async () => {
@@ -16,6 +17,17 @@ describe('Invoice routes', () => {
             .send()
         expect(res.statusCode).toEqual(200)
         expect(res.body.length).toBeGreaterThanOrEqual(1)
+    })
+
+    it('should invoice upload', async () => {
+        const res = await request(app)
+            .post('/invoice')
+            // NOTE: Can be replaced with dynamically generated csv file in memory, for better testing, someday
+            .attach('csv', path.resolve('tests', 'data', 'sample1.csv'))
+        expect(Object.keys(res.body)).toContain('failed')
+        expect(res.body.failed.length).toEqual(1)
+        expect(Object.keys(res.body)).toContain('passed')
+        expect(res.body.passed.length).toEqual(3)
     })
 
     // After all tests have finished, close the DB connection
