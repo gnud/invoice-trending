93b923703e1265aecf835e090756062112e8e447
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2020-11-18 22:05:35 +0100

    Generated app fixes for e2e tests

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('invoicer app is running!');
+    expect(page.getTitleText()).toEqual('Invoice Trender');
   });
 
   afterEach(async () => {
